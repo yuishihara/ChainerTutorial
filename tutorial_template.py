801f19c4a46b5b909b6e02ba1e180dccae55da56
Yu Ishihara <yuishihara1225@gmail.com>
2018-07-16 14:13:03 +0900

    Add template for tutorial codes

---
+++ tutorial_template.py
@@ -0,0 +1,17 @@
+import numpy as np
+import chainer
+from chainer.backends import cuda
+from chainer import Function, gradient_check, report, training, utils, Variable
+from chainer import datasets, iterators, optimizers, serializers
+from chainer import Link, Chain, ChainList
+import chainer.functions as F
+import chainer.links as L
+from chainer.training import extensions
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
